6460c690bf71796f8c1ce5fd38badba6d6c98f12
caderrabeth <76124496+caderrabeth@users.noreply.github.com>
2022-05-19 18:30:41 -0500

    Completed challenge 6

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -19,11 +20,11 @@ function logger() {
 // logger();
 
 // adding arguments / parameters
-function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//     // console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
 
 const appleJuice = fruitProcessor(5, 0);
 // console.log(appleJuice);
@@ -45,4 +46,76 @@ const age2 = calcAge2(1982);
 // console.log(age1, age2);
 
 // arrow functions
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1982);
+// console.log(age3);
 
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`
+}
+// console.log(yearsUntilRetirement(1982, 'Kevin'))
+// console.log(yearsUntilRetirement(1975, 'Bob'))
+
+// Functions calling other functions.
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    let applePieces = cutFruitPieces(apples);
+    let orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+    return juice;
+}
+
+fruitProcessor(2, 3);
+*/
+
+/* ------------------ //
+// Coding Challenge 4 //
+// ------------------ */
+/*
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgDolphins * 2 <= avgKoalas) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log(`No winners!`)
+    }
+}
+
+
+const dolphins1 = calcAverage(44, 22, 71);
+const koalas1 = calcAverage(65, 54, 49)
+checkWinner(dolphins1, koalas1);
+
+const dolphins2 = calcAverage(85, 54, 41);
+const koalas2 = calcAverage(23, 34, 27);
+checkWinner(dolphins2, koalas2);
+*/
+
+/* ------------------ //
+// Coding Challenge 5 //
+// ------------------ */
+
+/*
+const calcTip = amount => amount >= 50 && amount <= 300 ? amount * 0.15 : amount * 0.2;
+
+function calcTip2(amount) {
+    if (amount >= 50 && amount <= 300) {
+        return amount * 0.15;
+    } else {
+        return amount * 0.2;
+    }
+}
+
+let bills = [125, 555, 44];
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+*/
