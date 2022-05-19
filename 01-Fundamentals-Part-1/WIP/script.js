184afe6fb3582f9cc7e9e452d84acaff46d4774d
caderrabeth <76124496+caderrabeth@users.noreply.github.com>
2022-05-19 10:18:31 -0500

    Completed assignment for lesson 13

--- 01-Fundamentals-Part-1/WIP/script.js
+++ 01-Fundamentals-Part-1/WIP/script.js
@@ -1,4 +1,15 @@
 // Values and Variables Assignment
-let country = 'USA';
-let continenct = 'North America';
-let population = 329.5; 
+const country = 'USA';
+const continent = 'North America';
+let population = 329.5;
+
+// Data Types
+const isIsland = false;
+let language;
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof population);
+// console.log(typeof language)
+
+// let, const, and var
+language = 'English'
