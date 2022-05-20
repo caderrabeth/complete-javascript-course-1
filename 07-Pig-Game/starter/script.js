96d82f993f5bcbb78553add4fa3878f88796d0ef
caderrabeth <76124496+caderrabeth@users.noreply.github.com>
2022-05-20 14:04:39 -0500

    Added polish, getters, win conditions

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,77 +6,90 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const imgDice = document.querySelector('.dice');
 const players = document.querySelectorAll('.player');
-let currentScores = [0, 0];
-let totalScores = [0, 0];
-let activePlayer = 0;
+let currentScores, totalScores, activePlayer;
 
-// Setup new game
-const newGame = function () {
+// Getters
+const player = () => document.querySelector(`.player--${activePlayer}`);
+const currentScore = () => document.getElementById(`current--${activePlayer}`);
+const totalScore = () => document.getElementById(`score--${activePlayer}`);
+
+// Init Functions
+const hideDice = function () {
   imgDice.classList.add('hidden');
-  for (let i = 0; i < players.length; i++) {
-    players[i].querySelector('.score').textContent = 0;
-    players[i].querySelector('.current-score').textContent = 0;
-  }
-  if (!players[0].classList.contains('player--active')) changePlayer();
 };
 
-// Initialize game
-newGame();
-for (let i = 0; i < players.length; i++) {
-  score[i] = Number(players[i].querySelector('.score').textContent);
-  currentScore[i] = Number(
-    players[i].querySelector('.current-score').textContent
-  );
-}
+// Initialize
+const init = function () {
+  currentScores = [0, 0];
+  totalScores = [0, 0];
+  activePlayer = 0;
+
+  hideDice();
+  for (let i = 0; i < players.length; i++) {
+    document.getElementById(`score--${i}`).textContent = 0;
+    document.getElementById(`current--${i}`).textContent = 0;
+    players[i].classList.remove('player--winner', 'player--active');
+  }
+  players[0].classList.add('player--active');
+  btnRoll.classList.remove('hidden');
+  btnHold.classList.remove('hidden');
+};
+
+init();
 
 // Dice Roller
 const rollDice = function () {
   const diceRoll = Math.trunc(Math.random() * 6) + 1;
-  if (imgDice.classList.contains('hidden')) imgDice.classList.remove('hidden');
+
+  diceRoll !== 1 ? addScore(diceRoll) : changePlayer();
+
   imgDice.src = `dice-${diceRoll}.png`;
-  diceRoll !== 1 ? addScore(diceRoll) : addScore(0);
+  if (imgDice.classList.contains('hidden')) imgDice.classList.remove('hidden');
 };
 
 // Add value to active player's current score.
 const addScore = function (val) {
-  const currentScore = document
-    .querySelector('.player--active')
-    .querySelector('.current-score');
   if (val === 0) {
-    currentScore.textContent = 0;
-    changePlayer();
+    currentScores[activePlayer] = 0;
   } else {
-    const cval = Number(currentScore.textContent);
-    currentScore.textContent = cval + val;
+    currentScores[activePlayer] += val;
   }
-};
-
-// Bank active player's current score to total.
-const holdScore = function () {
-  const currentScore = document
-    .querySelector('.player--active')
-    .querySelector('.current-score');
-  const totalScore = document
-    .querySelector('.player--active')
-    .querySelector('.score');
-  if (Number(currentScore.textContent) === 0) return;
-  totalScore.textContent =
-    Number(currentScore.textContent) + Number(totalScore.textContent);
-  currentScore.textContent = 0;
-  changePlayer();
+  currentScore().textContent = currentScores[activePlayer];
 };
 
 // Change active player
 const changePlayer = function () {
-  for (let i = 0; i < players.length; i++) {
-    players[i].classList.contains('player--active')
-      ? players[i].classList.remove('player--active')
-      : players[i].classList.add('player--active');
-  }
-  activePlayer === 0 ? 1 : 0;
+  addScore((currentScores[activePlayer] = 0));
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  for (let i = 0; i < players.length; i++)
+    players[i].classList.toggle('player--active');
+};
+
+// Declare a winner
+const playerWin = function () {
+  player.classList.remove('player--active');
+  player.classList.add('player--winner');
+  btnRoll.classList.add('hidden');
+  btnHold.classList.add('hidden');
+};
+
+// Bank active player's current score to total.
+const holdScore = function () {
+  if (currentScores[activePlayer] === 0) return;
+
+  // update script
+  totalScores[activePlayer] += currentScores[activePlayer];
+  currentScores[activePlayer] = 0;
+
+  // update display
+  totalScore().textContent = totalScores[activePlayer];
+  currentScore().textContent = 0;
+  hideDice();
+  totalScores[activePlayer] >= 100 ? playerWin() : changePlayer();
 };
 
 // Event Listeners
 btnRoll.addEventListener('click', rollDice);
 btnHold.addEventListener('click', holdScore);
-btnNew.addEventListener('click', newGame);
+btnNew.addEventListener('click', init);
