303627c3b6498bfc8b7ecf130b607a776ca79631
caderrabeth <76124496+caderrabeth@users.noreply.github.com>
2022-05-20 10:32:59 -0500

    Completed Guess The Number project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,35 +1,60 @@
 'use strict';
 
-const target = Math.ceil(Math.random() * 20);
+let target;
+newTarget();
+let score = 20;
+let highscore = 0;
 
 const btnCheck = document.querySelector('.check');
-const userGuess = document.querySelector('.guess');
+const btnAgain = document.querySelector('.again');
 const txtNumber = document.querySelector('.number');
-let score = 20;
-
-txtNumber.textContent = target;
 
 btnCheck.addEventListener('click', function () {
-  const txtMessage = document.querySelector('.message');
-  const guess = Number(userGuess.value);
+  const guess = Number(document.querySelector('.guess').value);
 
   if (!score) return;
 
   if (!guess) {
-    txtMessage.textContent = 'No Number!';
+    setMessage('No Number!');
+  } else if (guess < 1 || guess > 20) {
+    setMessage('Out of Range!');
   } else if (guess === target) {
-    txtMessage.textContent = 'You Win!';
-  } else if (guess > target) {
-    txtMessage.textContent = 'Too High!';
+    setMessage('You Win!');
+    setBackground('#60b347');
+    txtNumber.textContent = target;
+    txtNumber.style.width = '30rem';
+  } else {
     updateScore(--score);
-  } else if (guess < target) {
-    txtMessage.textContent = 'Too Low!';
-    updateScore(--score);
+    guess > target ? setMessage('Too High!') : setMessage('Too Low!');
   }
 
   if (!score) txtMessage.textContent = 'You Lost!';
 });
 
-function updateScore(val) {
-  document.querySelector('.score').textContent = val;
+btnAgain.addEventListener('click', function () {
+  if (score > highscore) {
+    document.querySelector('.highscore').textContent = score;
+    highscore = score;
+  }
+
+  newTarget();
+  setBackground('#222');
+  txtNumber.textContent = '?';
+  txtNumber.style.width = '15rem';
+  score = 20;
+  updateScore(20);
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+});
+
+function newTarget() {
+  target = Math.ceil(Math.random() * 20);
 }
+
+const updateScore = val => (document.querySelector('.score').textContent = val);
+
+const setBackground = val =>
+  (document.querySelector('body').style.backgroundColor = val);
+
+const setMessage = val =>
+  (document.querySelector('.message').textContent = val);
