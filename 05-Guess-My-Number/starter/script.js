15436087126cea4ba07870f45e8adcbbd389346f
caderrabeth <76124496+caderrabeth@users.noreply.github.com>
2022-05-19 22:23:54 -0500

    Initial pass on project 05

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const target = Math.ceil(Math.random() * 20);
+
+const btnCheck = document.querySelector('.check');
+const userGuess = document.querySelector('.guess');
+const txtNumber = document.querySelector('.number');
+let score = 20;
+
+txtNumber.textContent = target;
+
+btnCheck.addEventListener('click', function () {
+  const txtMessage = document.querySelector('.message');
+  const guess = Number(userGuess.value);
+
+  if (!score) return;
+
+  if (!guess) {
+    txtMessage.textContent = 'No Number!';
+  } else if (guess === target) {
+    txtMessage.textContent = 'You Win!';
+  } else if (guess > target) {
+    txtMessage.textContent = 'Too High!';
+    updateScore(--score);
+  } else if (guess < target) {
+    txtMessage.textContent = 'Too Low!';
+    updateScore(--score);
+  }
+
+  if (!score) txtMessage.textContent = 'You Lost!';
+});
+
+function updateScore(val) {
+  document.querySelector('.score').textContent = val;
+}
